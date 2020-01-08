c03968e75940f021e1d42819b2e54a8ffd4aa514
Lucas Picoloto <lfpicoloto@gmail.com>
2020-01-08 10:13:40 -0300

    Feature/create models (#1)
    * created models
    
    * create route to test in heroku
    
    * add rest-framework
    
    * selecting the right database
    
    * set static files locally

---
+++ api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Athlete
+
+
+class AthleteSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = Athlete
+        fields = '__all__'
--- olympic_history_athletes/settings.py
+++ olympic_history_athletes/settings.py
@@ -33,19 +33,20 @@ ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'olympic-history-athletes.
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -78,15 +79,7 @@ WSGI_APPLICATION = 'olympic_history_athletes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
-
+DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -125,6 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'project_name/static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'olympic_history_athletes/static')]
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
