94ca429bfe027a4d6e1237fac4645b43e068c61e
lfpicoloto <lfpicoloto@gmail.com>
2020-01-08 01:58:07 -0300

    selecting the right database

--- olympic_history_athletes/settings.py
+++ olympic_history_athletes/settings.py
@@ -118,6 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'project_name/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'olympic_history_athletes/static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
