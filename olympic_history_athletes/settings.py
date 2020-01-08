5d7cc95971832501421de20597baca26bdb537dd
lfpicoloto <lfpicoloto@gmail.com>
2020-01-08 01:54:54 -0300

    selecting the right database

--- olympic_history_athletes/settings.py
+++ olympic_history_athletes/settings.py
@@ -33,13 +33,13 @@ ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'olympic-history-athletes.
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'api',
     'rest_framework',
 ]
